59091ebff931ef280a7270ac50f7cb2e2e158d01
Airmax21 <iqbal.agyan@gmail.com>
2022-08-07 07:57:58 +0700

    update

--- deploy-command.js
+++ deploy-command.js
@@ -3,15 +3,49 @@ const { REST } = require('@discordjs/rest')
 const dotenv = require('dotenv')
 dotenv.config()
 const commands = [
-    new SlashCommandBuilder().setName('help').setDescription('Menampilkan daftar perintah'),
-    new SlashCommandBuilder().setName('ping').setDescription('Balas Pong'),
-    new SlashCommandBuilder().setName('user').setDescription('Menampilkan info user'),
-    new SlashCommandBuilder().setName('yandere').setDescription('Mengirim gambar dari yandere'),
-    new SlashCommandBuilder().setName('nhentai').setDescription('Mengirim pdf dari nhentai'),
-    new SlashCommandBuilder().setName('absen').setDescription('Absen amikom'),
-    new SlashCommandBuilder().setName('tts').setDescription('Kirim vn')
+    new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Menampilkan daftar perintah'),
+    new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Balas Pong'),
+    new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('Menampilkan info user'),
+    new SlashCommandBuilder()
+        .setName('yandere')
+        .setDescription('Mengirim gambar dari yandere')
+        .addStringOption(Option => Option
+            .setName('search')
+            .setDescription('Nama Chara / Genre yang dicari')
+            .setRequired(true))
+        .addIntegerOption(Option => Option
+            .setName('jumlah')
+            .setDescription('Jumlah gambar yang diinginkan')),
+    new SlashCommandBuilder()
+        .setName('nhentai')
+        .setDescription('Mengirim pdf dari nhentai')
+        .addIntegerOption(Option => Option
+            .setName('kode')
+            .setDescription('Kode nhentai yang diinginkan')
+            .setRequired(true)),
+    new SlashCommandBuilder()
+        .setName('absen')
+        .setDescription('Absen amikom')
+        .addStringOption(Option => Option
+            .setName('nim')
+            .setDescription('Nim yang akan di presensi'))
+        .addStringOption(Option => Option
+            .setName('kode')
+            .setDescription('Kode presensi 5 digit'))
+        .addAttachmentOption(Option => Option
+            .setName('qr')
+            .setDescription('QRCode untuk presensi')),
+    new SlashCommandBuilder()
+        .setName('tts')
+        .setDescription('Kirim vn')
 ].map(command => command.toJSON())
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
 rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
-    .then(()=> console.log('Berhasil buat command'))
+    .then(() => console.log('Berhasil buat command'))
     .catch(console.error)
--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const {Client, GatewayIntentBits} = require('discord.js')
+const { Client, GatewayIntentBits } = require('discord.js')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const client = new Client({intents:[GatewayIntentBits.Guilds]})
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-client.once('ready',()=>{
+client.once('ready', () => {
     console.log('Berhasil login')
+})
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return
+
+    const { commandName } = interaction
+
+    switch (commandName) {
+        case 'ping':
+            await interaction.reply('Pong!')
+            break
+        case 'help':
+            await interaction.reply(
+                'Ping = Tes Bot\nUser = Menampilkan Info User\nYandere = Mengirim gambar sesuai chara/genre\n'
+            )
+        case 'user':
+        case 'yandere':
+        case 'nhentai':
+        case 'absen':
+        case 'tts':
+        case 'yt':
+        
+    }
 })
 client.login(process.env.TOKEN)
